69eda3041585ececfaccc3f293c23f88d8c76368
Ryan Liu <asdfryan123@gmail.com>
2013-03-16 15:21:35 -0400

    deleted lib and added event emitter to index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = require('./lib/');
+var EventEmitter = require('events').EventEmitter,
+	offlineEmitter = new EventEmitter,
+	emitOnline = function () {
+		offlineEmitter.emit("online");
+		console.log("online");
+	},
+	emitOffline = function () {
+		offlineEmitter.emit("offline");
+		console.log("offline");
+	};
+
+
+
+if (document.body.addEventListener) {
+	/*For Firefox, Opera, Chrome, Safari 
+	*/
+	document.body.addEventListener("online", emitOnline, false);
+	document.body.addEventListener("offline", emitOffline, false);
+}
+else {
+	/* For IE
+	*/
+	document.body.ononline = emitOnline;
+	document.body.onoffline = isOffline;
+}
